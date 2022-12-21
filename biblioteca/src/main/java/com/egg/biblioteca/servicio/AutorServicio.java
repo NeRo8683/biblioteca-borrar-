ebc6f5cb3e133d46da4b799a9616689f4ebb0040
“Cris <christian8683@gmail.com>
2022-12-20 23:06:34 -0300

    prueba

---
+++ biblioteca/src/main/java/com/egg/biblioteca/servicio/AutorServicio.java
@@ -0,0 +1,48 @@
+package com.egg.biblioteca.servicio;
+
+import com.egg.biblioteca.entidades.Autor;
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.repositorios.AutorRepositorio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorServicio {
+
+    @Autowired
+    AutorRepositorio autorRepositorio;
+
+    @Transactional
+    public void crearAutor(String nombre) throws MiException {
+        validar(nombre);
+        Autor autor = new Autor();
+        autor.setNombre(nombre);
+        autorRepositorio.save(autor);
+    }
+
+    public List<Autor> listarAutores() {
+        List<Autor> autores = new ArrayList();
+        autores = autorRepositorio.findAll();
+        return autores;
+    }
+
+    public void modificarAutor(String nombre, String id) {
+        Optional<Autor> respuesta = autorRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+            Autor autor = respuesta.get();
+            autor.setNombre(nombre);
+            autorRepositorio.save(autor);
+        }
+    }
+
+    private void validar(String nombre) throws MiException {
+        if (nombre.isEmpty() || nombre == null) {
+            throw new MiException("el nombre no puede ser nulo o vacio");
+        }
+    }
+
+}
